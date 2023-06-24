123d00d3b2a8be750361775f845c8e5a93554af1
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-24 09:50:39 +0300

    node_getting_started

--- Node_getting_started/env.js
+++ Node_getting_started/env.js
@@ -5,3 +5,10 @@ console.log(process.env.NAME);
 console.log(process.env.PROFESSION);
 console.log("I am learning the fundamentals of", process.env.COURSE);
 
+/*
+ubuntu@ubuntu-HP:~/Desktop/environments/Node_JS_Fundamentals/Node_getting_started$ node env.js
+Joab
+Software Engineer
+I am learning the fundamentals of Node.js
+ubuntu@ubuntu-HP:~/Desktop/environments/Node_JS_Fundamentals/Node_getting_started$ 
+*/
--- Node_getting_started/repl.js
+++ Node_getting_started/repl.js
@@ -6,3 +6,12 @@ local.on("exit", () => {
     console.log("... exiting REPL. Bye!");
     process.exit();
 });
+
+/*
+ubuntu@ubuntu-HP:~/Desktop/environments/Node_JS_Fundamentals/Node_getting_started$ node repl.js
+>> 
+(To exit, press ^C again or ^D or type .exit)
+>> 
+... exiting REPL. Bye!
+ubuntu@ubuntu-HP:~/Desktop/environments/Node_JS_Fundamentals/Node_getting_started$ 
+*/
